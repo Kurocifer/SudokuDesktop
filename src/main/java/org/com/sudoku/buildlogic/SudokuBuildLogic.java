d217039f109196548eedece298b8fa4465ad9b7c
Cifer-ti <draculemihawk232@gmail.com>
2024-04-06 09:54:51 +0100

    Add build logic for Sudoku Game

--- src/main/java/org/com/sudoku/buildlogic/SudokuBuildLogic.java
+++ src/main/java/org/com/sudoku/buildlogic/SudokuBuildLogic.java
@@ -1,2 +1,34 @@
-package org.com.sudoku.buildlogic;public class SudokuBuildLogic {
+package org.com.sudoku.buildlogic;
+
+import org.com.sudoku.computationlogic.GameLogic;
+import org.com.sudoku.persistence.LocalStorageImpl;
+import org.com.sudoku.problemdomain.SudokuGame;
+import org.com.sudoku.userinterface.IUserInterfaceContract;
+import org.com.sudoku.userinterface.logic.ControlLogic;
+import org.com.sudoku.problemdomain.IStorage;
+
+import java.io.IOException;
+public class SudokuBuildLogic {
+
+  public static void build(IUserInterfaceContract.View userInterface) throws IOException {
+    SudokuGame initialState;
+    IStorage storage = new LocalStorageImpl();
+
+    try {
+      //will throw if no game data is found in local storage
+
+      initialState = storage.getGameData();
+    } catch (IOException e) {
+
+      initialState = GameLogic.getNewGame();
+      //this method below will also throw an IOException
+      //if we cannot update the game data. At this point
+      //the application is considered unrecoverable
+      storage.updateGameData(initialState);
+    }
+
+    IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
+    userInterface.setListener(uiLogic);
+    userInterface.updateBoard(initialState);
+  }
 }
